7c88297b57ef391f0654da2f58e13adcc1a89a6a
Ricardo Pereira <ricardo.pereira@exceltic.com>
2020-12-26 12:38:05 +0100

    copia dos ficheros iniciais do projecto

---
+++ main2.py
@@ -0,0 +1,20 @@
+from smtplib import SMTP
+
+address_to_test = "ricardo.preira@exceltic.com"
+
+try:
+    with SMTP('gmail-smtp-in.l.google.com') as smtp:
+        host_exists = True
+        smtp.helo() # send the HELO command
+        smtp.mail('ricardo.pereira@exceltic.com') # send the MAIL command
+        resp = smtp.rcpt(address_to_test)
+        if resp[0] == 250: # check the status code
+            deliverable = True
+        elif resp[0] == 550:
+            deliverable = False
+        else:
+            print(resp[0])
+except smtplib.SMTPServerDisconnected as err:
+    print("SMTP connection error")
+
+print(deliverable)
---
+++ main3.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+a = np.array([[31, 1],[140,1]])
+b = np.array([7500, 0])
+
+print(np.linalg.solve(a,b))
